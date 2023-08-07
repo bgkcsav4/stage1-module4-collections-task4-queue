6593a1585967426d484fc4ae938f9e94557cdfa6
Latsa <bgkcsavvy@gmail.com>
2023-08-07 11:50:16 +0200

    done

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,36 +1,45 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
-import java.util.List;
 import java.util.Queue;
 
 public class ArrayDequeCreator {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
         ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
-        int everyDishNumberToEat = 2; // Set the number of dishes to eat in each turn
 
         while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
-            for (int i = 0; i < everyDishNumberToEat; i++) {
-                if (!firstQueue.isEmpty()) {
-                    arrayDeque.add(firstQueue.poll());
-                }
-                if (!secondQueue.isEmpty()) {
-                    arrayDeque.add(secondQueue.poll());
-                }
+            arrayDeque.add(firstQueue.poll()); // Player 1 takes the top card from the ArrayDeque and adds it to their queue.
+            arrayDeque.add(secondQueue.poll()); // Player 2 takes the top card from the ArrayDeque and adds it to their queue.
+
+            // Player 1 puts two cards from the beginning of their queue into the ArrayDeque.
+            if (!firstQueue.isEmpty()) {
+                arrayDeque.add(firstQueue.poll());
+            }
+            if (!firstQueue.isEmpty()) {
+                arrayDeque.add(firstQueue.poll());
+            }
+
+            // Player 2 puts two cards from the beginning of their queue into the ArrayDeque.
+            if (!secondQueue.isEmpty()) {
+                arrayDeque.add(secondQueue.poll());
+            }
+            if (!secondQueue.isEmpty()) {
+                arrayDeque.add(secondQueue.poll());
             }
         }
-
-        // Add any remaining dishes from the firstQueue
+/*
+        // Add any remaining cards from the firstQueue
         while (!firstQueue.isEmpty()) {
             arrayDeque.add(firstQueue.poll());
         }
 
-        // Add any remaining dishes from the secondQueue
+        // Add any remaining cards from the secondQueue
         while (!secondQueue.isEmpty()) {
             arrayDeque.add(secondQueue.poll());
         }
-
+*/
         return arrayDeque;
     }
 }
+
 
