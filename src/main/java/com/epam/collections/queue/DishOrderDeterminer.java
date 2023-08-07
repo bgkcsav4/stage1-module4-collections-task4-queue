bb7ec586f9b0026161955206c9bcad393e316694
Latsa <bgkcsavvy@gmail.com>
2023-08-07 11:27:08 +0200

    done

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,30 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> dishOrder = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Initialize the queue with dish numbers from 1 to numberOfDishes
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+
+        while (!queue.isEmpty()) {
+            // Eat everyDishNumberToEat - 1 dishes
+            for (int i = 0; i < everyDishNumberToEat - 1; i++) {
+                queue.add(queue.poll());
+            }
+
+            // Eat the everyDishNumberToEat-th dish and remove it from the queue
+            dishOrder.add(queue.poll());
+        }
+
+        return dishOrder;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,19 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
-
+import java.util.ArrayDeque;
+import java.util.Queue;
 public class PriorityQueueCreator {
-    public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
-    }
-}
+    public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            arrayDeque.add(firstQueue.poll()); // Player 1 takes the top card from the ArrayDeque and adds to their queue.
+            arrayDeque.add(secondQueue.poll()); // Player 2 takes the top card from the ArrayDeque and adds to their queue.
+            arrayDeque.add(firstQueue.poll()); // Player 1 puts two cards from the beginning of their queue into the ArrayDeque.
+            arrayDeque.add(firstQueue.poll());
+            arrayDeque.add(secondQueue.poll()); // Player 2 puts two cards from the beginning of their queue into the ArrayDeque.
+            arrayDeque.add(secondQueue.poll());
+        }
+
+        return arrayDeque;
+    }}
